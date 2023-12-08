2c37aac4572cad0612c0cab95a1e63566af49fbc
Raul Pop <152633577+onboardingconsultant@users.noreply.github.com>
2023-12-08 21:24:53 +0100

    fixes

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ function changeText(buttonId) {
     var clickedButton = document.getElementById(buttonId);
 
     if (clickedButton.classList.contains('active')) {
-        // If the button is already active, toggle it off
-        clickedButton.classList.remove('active');
-        hideText(buttonId);
+        // Do nothing if the button is already active
     } else {
         // If the button is not active, toggle it on
         var buttons = document.querySelectorAll('.custom-button');
@@ -24,20 +22,53 @@ function changeText(buttonId) {
 
         // Add additional text when the "Users" button is clicked
         if (buttonId === 'users') {
-            var additionalText = document.getElementById('additionalText');
+            var additionalText = document.getElementById('additionalTextUsers');
             additionalText.innerHTML = `
-                <p>Define the onboarding team and roles.</p>
+                <p>Users: Define the onboarding team and roles.</p>
                 <p>Conduct internal training sessions on the importance of continuous onboarding.</p>
                 <!-- ... (add the rest of the text here) ... -->
                 <p>Close the project and transition to ongoing onboarding maintenance and improvement.</p>
             `;
         }
 
+        // Add additional text when the "New Hires" button is clicked
+        if (buttonId === 'newHires') {
+            var additionalText = document.getElementById('additionalTextnewHires');
+            additionalText.innerHTML = `
+                <p>New Hires: Define the onboarding team and roles.</p>
+                <p>Conduct internal training sessions on the importance of continuous onboarding.</p>
+                <!-- ... (add the rest of the text here) ... -->
+                <p>Close the project and transition to ongoing onboarding maintenance and improvement.</p>
+            `;
+        }
+
+        // Add additional text when the "Partners" button is clicked
+        if (buttonId === 'partners') {
+            var additionalText = document.getElementById('additionalTextPartners');
+            additionalText.innerHTML = `
+                <p>Partners: Define the onboarding team and roles.</p>
+                <p>Conduct internal training sessions on the importance of continuous onboarding.</p>
+                <!-- ... (add the rest of the text here) ... -->
+                <p>Close the project and transition to ongoing onboarding maintenance and improvement.</p>
+            `;
+        }
+
+        // Add additional text when the "Customers" button is clicked
+        if (buttonId === 'customers') {
+            var additionalText = document.getElementById('additionalTextCustomers');
+            additionalText.innerHTML = `
+                <p>Customers: Define the onboarding team and roles.</p>
+                <p>Conduct internal training sessions on the importance of continuous onboarding.</p>
+                <!-- ... (add the rest of the text here) ... -->
+                <p>Close the project and transition to ongoing onboarding maintenance and improvement.</p>
+            `;
+        }
     }
 }
 
-function hideText(buttonId) {
-    // Function to hide the text section
-    var textArea = document.getElementById(buttonId + 'Text');
-    textArea.style.display = 'none';
-}
+document.addEventListener("DOMContentLoaded", function () {
+    var newHiresButton = document.getElementById('newHires');
+    if (newHiresButton) {
+        newHiresButton.click();
+    }
+});
